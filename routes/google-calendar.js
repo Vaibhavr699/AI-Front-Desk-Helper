4eb5dac611018533cd282c127e8c3675676f8b45
vaibhavr699 <vaibhavr699@gmail.com>
2026-03-20 08:33:29 +0100

    fix: db import path in google-calendar routes

--- routes/google-calendar.js
+++ routes/google-calendar.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const { google } = require("googleapis");
-const db = require("../db");
+const db = require("../lib/db");
 const router = express.Router();
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];

267e2674751675ddeca8fbc1e5a54ed855d93dbb
vaibhavr699 <vaibhavr699@gmail.com>
2026-03-27 12:34:17 -0500

    Adjust z-index and pointer events in container-scroll-animation

--- dashboard/src/components/ui/container-scroll-animation.jsx
+++ dashboard/src/components/ui/container-scroll-animation.jsx
@@ -41,7 +41,7 @@ export const ContainerScroll = ({
                     perspective: "1000px",
                 }}
             >
-                <div className="relative z-[200] pointer-events-auto">
+                <div className="relative z-[10] pointer-events-auto">
                     <Header translate={translate} titleComponent={titleComponent} />
                 </div>
                 <Card rotate={rotate} translate={translate} scale={scale}>
@@ -79,7 +79,7 @@ export const Card = ({
                 boxShadow:
                     "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
             }}
-            className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl z-10"
+            className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl z-10 pointer-events-auto"
         >
             <div className="h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
                 {children}

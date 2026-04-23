e824ee11710819d526b8f9c45fe4699a0b672ef8
vaibhavr699 <vaibhavr699@gmail.com>
2026-04-23 16:22:57 -0500

    Update rollup route to use rollupV5

--- server.js
+++ server.js
@@ -431,7 +431,7 @@ app.use("/api/coaching", authMiddleware, require("./routes/coaching"));
 app.use("/api/ai-coach", authMiddleware, require("./routes/aicoach"));
 app.use("/api/reviews", require("./routes/reviews"));
 app.use('/api/reseller', require('./routes/reseller'));
-app.use('/api/rollup', authMiddleware, require('./routes/rollup'));
+app.use('/api/rollup', authMiddleware, require('./routes/rollupV5'));
 app.use('/churn', require('./routes/churnPublic'));
 
 // Serve dashboard static assets early so JS/CSS/images load,

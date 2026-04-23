413ed8395bc7b8314bdd2835a4aced253ebdc2b2
vaibhavr699 <vaibhavr699@gmail.com>
2026-04-23 17:04:58 -0500

    Update index.js

--- dashboard/src/pages/index.js
+++ dashboard/src/pages/index.js
@@ -25,3 +25,4 @@ export { default as Outbound } from "./Outbound";
 export { default as Locations } from "./Locations";
 export { default as FranchiseeInvite } from "./FranchiseeInvite";
 export { default as Welcome } from "./Welcome";
+export { default as RollupV5 } from "./RollupV5";

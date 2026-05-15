5cb982235d613bbc7c43237cf69e7166c1c991ba
vaibhavr699 <vaibhavr699@gmail.com>
2026-05-15 06:21:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -532,6 +532,7 @@ app.use("/api/google-calendar", authMiddleware, require("./routes/google-calenda
 app.use("/webhooks", require("./routes/webhooks"));
 app.use("/api/coaching", authMiddleware, require("./routes/coaching"));
 app.use("/api/ai-coach", authMiddleware, require("./routes/aicoach"));
+app.use("/api/coaching-test", require("./routes/coachingTest"));
 app.use('/api/rollup-v5', authMiddleware, require('./routes/rollupV5'));
 app.use("/api/scope-options", authMiddleware, require("./routes/scopeOptions"));
 app.use("/api/vertical-services", authMiddleware, require("./routes/verticalServices"));

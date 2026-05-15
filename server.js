1f3ea9261c134e4fad5690dc869be66749af9916
vaibhavr699 <vaibhavr699@gmail.com>
2026-05-15 06:43:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -525,6 +525,7 @@ app.use("/api/reviews", require("./routes/reviews"));
 app.use("/", require("./routes/estimateLink"))
 app.use("/api/tenants", authMiddleware, require("./routes/serviceArea"));
 app.use('/api/reseller', require('./routes/reseller'));
+app.use("/api/coaching-test", require("./routes/coachingTest"));
 app.use("/api/public/branding", require("./routes/publicBranding"));
 app.use("/api", authMiddleware, dashboardRoutes);
 app.use("/auth/google/calendar", require("./routes/google-calendar"));
@@ -532,7 +533,6 @@ app.use("/api/google-calendar", authMiddleware, require("./routes/google-calenda
 app.use("/webhooks", require("./routes/webhooks"));
 app.use("/api/coaching", authMiddleware, require("./routes/coaching"));
 app.use("/api/ai-coach", authMiddleware, require("./routes/aicoach"));
-app.use("/api/coaching-test", require("./routes/coachingTest"));
 app.use('/api/rollup-v5', authMiddleware, require('./routes/rollupV5'));
 app.use("/api/scope-options", authMiddleware, require("./routes/scopeOptions"));
 app.use("/api/vertical-services", authMiddleware, require("./routes/verticalServices"));
